60a9be41a567d6020708d3fc8e995ece230ddbf3
kang1245 <s34958171@gmail.com>
2024-03-27 14:26:28 +0800

    21

---
+++ 5/app-files/data.js
@@ -0,0 +1,268 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "貝加爾",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.5415851117401154,
+        "pitch": 0.09221223157683944,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9020144893763735,
+          "pitch": -0.02286108022405209,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.6167615185755553,
+          "pitch": 0.02948022763739644,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8814967991723517,
+          "pitch": -0.25195848706133717,
+          "title": "貝加爾湖",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "東京車站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.6581548598336404,
+        "pitch": -0.039011046731371124,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5494625175209507,
+          "pitch": -0.052083055060538896,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.553989015933892,
+          "pitch": -0.057931394123031055,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -3.0409207753213785,
+          "pitch": -0.022180793634333895,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.5604120858825006,
+          "pitch": -0.022611817847867144,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.370152799636747,
+          "pitch": -0.27537358413616175,
+          "title": "東京車站",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "廣場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.688621072725498,
+        "pitch": -0.06916939942181166,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.574345235419699,
+          "pitch": -0.023229744904167404,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4584104776703946,
+          "pitch": -0.1332500516337749,
+          "title": "時代廣場",
+          "text": "時代的潮流"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "羅馬",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20118858872029932,
+          "pitch": 0.020295887216963848,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 3.0676156326811395,
+          "pitch": 0.018301179181582583,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8305994668017185,
+          "pitch": 0.06964372745070335,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "羅馬競技場",
+          "text": "打架的地方"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "聖家堂",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -3.1204124096193944,
+          "pitch": 0.02612461982431391,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6291666905120046,
+          "pitch": -0.7918410780124603,
+          "title": "聖家堂",
+          "text": "大教堂"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ 首頁/app-files/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-street-view-360",
+      "name": "Street View 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5333549003680744,
+        "pitch": -0.0010879923510493938,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
